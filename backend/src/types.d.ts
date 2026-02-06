8245cffdb148ce2a5aa79369e327598534963a90
Michael Ruggeri <michaelruggeri@192.168.1.56>
2026-02-06 13:12:05 +0100

    fix typings for jwt and bcrypt

--- backend/src/types.d.ts
+++ backend/src/types.d.ts
@@ -1,14 +1,23 @@
 import "fastify";
+import "@fastify/jwt";
 
-declare module "fastify" {
-  interface FastifyInstance {
-    authenticate: any;
-  }
-  interface FastifyRequest {
-    user?: {
+declare module "@fastify/jwt" {
+  interface FastifyJWT {
+    payload: {
       id: string;
       role: string;
       companyId?: string | null;
     };
+    user: {
+      id: string;
+      role: string;
+      companyId?: string | null;
+    };
+  }
+}
+
+declare module "fastify" {
+  interface FastifyInstance {
+    authenticate: any;
   }
 }
